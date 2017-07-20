f08221b3353fd79bba01a9e72e1e2fb4eb8a13df
April Ross <aprilross@Aprils-MacBook-Pro.local>
2017-07-19 21:16:17 -0400

    finalcommit

--- src/com/company/GamePiece.java
+++ src/com/company/GamePiece.java
@@ -3,17 +3,17 @@ package com.company;
 /**
  * Created by aprilross on 7/18/17.
  */
-public class GamePiece {
+class gamePiece {
     int positionX;
     int positionY;
-    int deltaX;
-    int deltaY;
+    int newX;
+    int newY;
     boolean frozen;
     String name;
     String color;
 
 
-    public GamePiece() {
+    public gamePiece() {
         positionX = 0;
         positionY = 0;
         frozen = false;
@@ -47,28 +47,50 @@ public class GamePiece {
         return color;
     }
 
-    public void move(int deltaX, int deltaY)
-        this.positionX += deltaX;
-        this.positionY += deltaY;
+    public void move(int newX, int newY) {
+        if (frozen != true) {
+            this.positionX = newX;
+            this.positionY = newY;
 
+        }
     }
 
-     public void freeze(boolean frozen){
+    public void freeze(boolean frozen) {
+
         this.frozen = true;
-     }
+    }
 
-     public void unfreeze(boolean frozen){
-         this.frozen = false;
-     }
+    public void unfreeze(boolean frozen) {
+        this.frozen = false;
+    }
 
-     public static void main (String[] args){
-         GamePiece piece = new GamePiece();
-
-         piece.getPositionX();
-         piece.getPositionY();
-         System.out.println("The piece is in x-position and y-position:" + positionX "," + positionY);
-
-     }
 }
+ public class GamePiece {
+     public static void main(String[] args) {
+         gamePiece Rook = new gamePiece();
+         Rook.name = "Rook";
+         Rook.color = "Black";
+         int x = Rook.getPositionX();
+         int y = Rook.getPositionY();
+
+
+         System.out.println("The Rook is in the x-position" + x + "and in the y-position" + y);
+
+         if(Rook.isFrozen()){
+             System.out.println("The Rook is frozen and can't move.");
+         }
+         else{
+             System.out.println("The Rook is not frozen and able to move.");
+         }
+
+         Rook.move(35, 25);
+         System.out.println("The Rook is in x-position and y-position:" + Rook.positionX + "," + Rook.positionY);
+
+
+
+     }
+ }
+
+
 
 
